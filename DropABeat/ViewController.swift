d912727fbe1845f0e50925225b9ca36947f93e23
Alex Raman <raman.alex@gmail.com>
2015-07-08 10:38:24 -0700

    Initial Commit

---
+++ DropABeat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DropABeat
+//
+//  Created by Alex Raman on 7/8/15.
+//  Copyright (c) 2015 Alex Raman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
